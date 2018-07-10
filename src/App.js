bb45b8e372aaa064eb4b2da6f865694eb7f18aed
Jonjo McKay <jonjo.mckay@manywho.com>
2018-07-10 10:16:32 +0100

    Changed entry point flow

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
     render() {
         return (
             <Provider store={ store }>
-                <Flow id="b7e2a056-35dd-4973-b279-c85aeafb299c" tenant="4dc56bec-b68c-47ac-8998-7ba961734c18" />
-                {/*<Flow id="c6b211f0-b342-4f67-b7b1-b8b62868b2a4" tenant="07f799a4-af7c-449b-ba7c-f1f526f7000a" />*/}
+                {/*<Flow id="b7e2a056-35dd-4973-b279-c85aeafb299c" tenant="4dc56bec-b68c-47ac-8998-7ba961734c18" />*/}
+                <Flow id="c6b211f0-b342-4f67-b7b1-b8b62868b2a4" tenant="07f799a4-af7c-449b-ba7c-f1f526f7000a" />
             </Provider>
         );
     }
